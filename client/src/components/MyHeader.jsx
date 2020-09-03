70df814e8df9cc448d12521ad2cc9d35ba562044
DenisDaniel707 <ardelean_denis707@yahoo.com>
2020-09-03 21:39:43 +0300

    Updated Header

--- client/src/components/MyHeader.jsx
+++ client/src/components/MyHeader.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import '../../node_modules/antd/dist/antd.css';
-import { Layout, Menu } from 'antd';
+import { Layout, Divider } from 'antd';
 import logo from './logo.png'
 
-const { Header } = Layout;
 const MyHeader = () => {
     return (
-        <Header className="header">
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1" style={{margin: '0 30px'}} ><img src={logo} width="60" height="50" alt="Logo" /></Menu.Item>
-        </Menu>
-        </Header>
+        <Layout>
+            <div style={{backgroundColor: "#0a2b59", padding: "0 4%", height: "70px"}} align="left">
+                <img src={logo} width="75" height="53" alt="Logo" style={{padding: "-10px 0", position: "relative", top: "11%"}}/>
+                <Divider type="vertical"/>
+            </div>
+        </Layout>
     )
 }
 
